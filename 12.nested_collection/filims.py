088796ee686a18a3b2b6126deed38f1ff177e7c6
Rebin <muhammedrebin1794@gmail.com>
2024-07-30 16:27:42 +0530

    Extra problems done on film data

--- 12.nested_collection/filims.py
+++ 12.nested_collection/filims.py
@@ -67,16 +67,19 @@ movies = [
     }
 ]
 
+#------------------------------------------------------------------------------------------------------------------------------------------- 
 # q1) total_number_of_movies
 
 movies_count = len(movies)
 # print(movies_count)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q2) movies with genre Drama
 
 malayalam_movies = [mov.get("title") for mov in movies if "Drama" in mov.get("genres")]
 # print(malayalam_movies)
-        
+
+#-------------------------------------------------------------------------------------------------------------------------------------------       
 # q3) latest movie
 
 def get_year(mov):
@@ -86,6 +89,7 @@ latest_year_movie = max(movies, key=get_year)
 latest_movies = [mov.get("title") for mov in movies if mov.get("year") == latest_year_movie.get("year")]
 # print(latest_movies)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q4) top movie (movie with highest rating)
 
 def get_rating(mov):
@@ -95,28 +99,36 @@ high_rated_movie = max(movies, key=get_rating)
 top_movies = [mov.get("title") for mov in movies if mov.get("rating") == high_rated_movie.get("rating")]
 # print(top_movies)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q5) movies with language malayalam
 
 malayalam_movies = [mov.get("title") for mov in movies if "Malayalam" == mov.get("language")]
 # print(malayalam_movies)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q6) movies released after year 2016
 
 movies_after_2016 = [mov.get("title") for mov in movies if mov.get("year") > 2016]
 # print(movies_after_2016)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q7) movie with lowest rating
 
 low_rated_movie = min(movies, key=get_rating)
 low_movies = [mov.get("title") for mov in movies if mov.get("rating") == low_rated_movie.get("rating")]
 # print(low_movies)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q8) malayalam movies with genre Action
 
 malayalam_movies_action = [mov.get("title") for mov in movies if "Action" in mov.get("genres") and "Malayalam" == mov.get("language")]
 # print(malayalam_movies_action)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q9) movies released in same years
+
+# Approach 1: but only work for first occuring repeated year
+
 def get_repeating_year(movies):
     for i in range(len(movies)):
         for j in range(i+1,len(movies)):
@@ -128,14 +140,67 @@ repeating_year = get_repeating_year(movies)
 
 same_year_movies = [mov.get("title") for mov in movies if repeating_year == mov.get("year")]
 # print(same_year_movies)
+# -----------------------------
+# Approach 2: Using dictionary.
 
+movie_dictionary = {}
+
+for m in movies:
+    
+    released_year = m.get("year")
+    
+    if released_year in movie_dictionary:
+        movie_dictionary.get(released_year).append(m.get("title"))
+    else:
+        movie_dictionary[released_year] = [m.get("title")]
+    
+# print(movie_dictionary)
+    
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q10) oldest movie
 
 oldest_movie_data = min(movies, key=get_year)
 oldest_movies = [mov.get("title") for mov in movies if mov.get("year") == oldest_movie_data.get("year")]
 # print(oldest_movies)
 
+#-------------------------------------------------------------------------------------------------------------------------------------------
 # q11) movie name with genres either Drama or Comedy
 
 movies_filter = [mov.get("title") for mov in movies if "Drama" in mov.get("genres") or "Comedy" in mov.get("genres")]
 # print(movies_filter)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+# q12) Number of movies released in each year
+
+years = [m.get("year") for m in movies]
+
+year_count = {y:years.count(y) for y in years}
+# print(year_count)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+# q13) list of genres
+
+# genre_set = set()
+# for m in movies:
+#     for g in m.get("genres"):
+#         genre_set.add(g)
+# print(genre_set)
+
+genres = {g for m in movies for g in m.get("genres")}
+# print(list(genres))
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+# q14) Number of movies in each genres
+
+all_genres = [g for m in movies for g in m.get("genres")]
+
+genres_movie_count = {g:all_genres.count(g) for g in all_genres}
+# print(genres_movie_count)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+# q15) list movie title in alphabetic order
+
+sorted_movies = sorted(movies, key= lambda mov:mov.get("title"))
+
+sorted_movies_titles = [mov.get("title") for mov in sorted_movies]
+print(sorted_movies_titles)
