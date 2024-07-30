52c44b64535fc734cfa18ee3fad76159814d095d
Rebin <muhammedrebin1794@gmail.com>
2024-07-30 08:04:49 +0530

    Familiarized: for else

---
+++ practice/python_basics/for_else.py
@@ -0,0 +1,8 @@
+# The else keyword in a for loop specifies a block of code to be executed when the loop is finished:
+
+tuple1 = (2,56,34,3,5,-1)
+
+for i in tuple1:
+    print(i)
+else:
+    print("Loop successfully completed and we are in the else block now.")
---
+++ practice/python_basics/for_else2.py
@@ -0,0 +1,7 @@
+tuple1 = (2,56,34,3,5,-1)
+
+for i in tuple1:
+    if i%6 == 0:
+        break
+else:
+    print("There is no number divisible by 6 in this sequence.")
