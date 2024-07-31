e12f7a65209d01a39fb9cc1186b216d45987e825
Rebin <muhammedrebin1794@gmail.com>
2024-07-31 07:35:02 +0530

    Done extra problems on olympics data

--- 12.nested_collection/olympics.py
+++ 12.nested_collection/olympics.py
@@ -84,4 +84,48 @@ def get_ratio(c):
     return ratio
 
 highest_ratio = max(olympic_medal_count, key=get_ratio)
-print(highest_ratio)
+# print(highest_ratio)
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# 11) Calculate the median number of bronze medals won by the countries.
+
+bronze_medels = sorted([c.get("bronze") for c in olympic_medal_count])
+index = int(len(bronze_medels)/2-1)
+
+if len(bronze_medels)%2 != 0:
+    median = bronze_medels[index+1]
+else:
+    median = (bronze_medels[index]+bronze_medels[index+1])/2
+    
+# print(median)
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# 12) What percentage of the total gold medals was won by the United States?
+
+gold_medal_list = [c.get("gold") for c in olympic_medal_count]
+total_gold_medals = sum(gold_medal_list)
+
+for c in olympic_medal_count:
+    if c.get("country") == "United States":
+        gold_US = c.get("gold")
+        
+percentage = (gold_US/total_gold_medals) * 100
+# print(f"{round(percentage, 2)}%")
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# 13) Create a list of countries that won at least 10 medals of each type (gold, silver, bronze).
+
+min_ten_medals = [c.get("country") for c in olympic_medal_count if c.get("gold") >= 10 and c.get("silver") >= 10 and c.get("bronze") >= 10]
+# print(min_ten_medals)
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# 14) Determine the country with the most balanced medal distribution (smallest difference between the highest and lowest medal counts).
+
+def get_medels_range(c):
+    m = [c.get("gold"), c.get("silver"), c.get("bronze")]
+    return max(m) - min(m)
+
+balanced_medal_country = min(olympic_medal_count, key=get_medels_range)
+# print(balanced_medal_country)
+
