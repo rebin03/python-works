0735e4fdfbb58a6b0434a20de98f1cecfbb64fac
Rebin <muhammedrebin1794@gmail.com>
2024-08-05 22:48:27 +0530

    Familiarized with packages

---
+++ 16.packages/my_program.py
@@ -0,0 +1,6 @@
+from operations import add,sub,factorial
+
+print(add(10,20))
+print(sub(40,20))
+
+print(factorial(4))
---
+++ 16.packages/operations.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def factorial(num):
+    result = 1
+    for i in range(1,num+1):
+        result *= i
+    return result
