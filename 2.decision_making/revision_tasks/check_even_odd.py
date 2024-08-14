25c38e1976b716424b2f9cea207643861b230add
Rebin <muhammedrebin1794@gmail.com>
2024-08-14 22:34:38 +0530

    Changed directory and name of directory

--- 2.decision_making/revision_tasks/check_even_odd.py
+++
@@ -1,9 +0,0 @@
-def even_or_odd(num):
-    if num%2 == 0:
-        print("Number is even")
-    else:
-        print("Number is odd")
-        
-
-num = int(input("Enter number: "))
-even_or_odd(num)
--- 2.decision_making/revision_tasks/check_voting_eligibility.py
+++
@@ -1,9 +0,0 @@
-def is_eligible_for_voting(age):
-    
-    if age >= 18:
-        print("Ypu are eligible for voting")
-    else:
-        print("You are not eligible for voting")
-        
-age = int(input("Enter your age: "))
-is_eligible_for_voting(age)
