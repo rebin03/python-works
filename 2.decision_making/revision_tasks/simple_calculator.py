639ca6ec726970f9e823caa5062e7e352d3f3cdf
Rebin <muhammedrebin1794@gmail.com>
2024-08-14 13:38:36 +0530

    Problem: grade classification, simple calc done

---
+++ 2.decision_making/revision_tasks/simple_calculator.py
@@ -0,0 +1,32 @@
+def add(n1,n2):
+    return n1+n2
+def subtract(n1,n2):
+    return n1-n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
+
+def calculator(num1,num2):
+    
+    op = input("\n+ for addition\n- for subtraction\n* for multiplication\n/ for division\nSelect operator: ")
+    
+    if op == "+":
+        res = add(num1,num2)
+    elif op == "-":
+        res = subtract(num1,num2)
+    elif op == "*":
+        res = multiply(num1,num2)
+    elif op == "/":
+        res = divide(num1,num2)
+    else:
+        print("invalid operator")
+
+    return res
+
+num1, num2 = map(int, input("Enter two numbers: ").split())
+result = calculator(num1,num2)
+print(f"Result is :{result}")
+
+    
+    
