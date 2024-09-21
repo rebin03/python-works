c2b229a52cb48af4e01dac1395e2ca3294f6b43f
Rebin <muhammedrebin1794@gmail.com>
2024-09-21 15:17:27 +0530

    Problem: integer come in all size

---
+++ practice/basic_problem/integer_come_in_all_size.py
@@ -0,0 +1,14 @@
+# Integers in Python can be as big as the bytes in your machine's memory.
+# There is no limit in size.
+
+# As we know, the result of a^b  grows really fast with increasing b.
+
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+res = a**b + c**d
+print(res)
