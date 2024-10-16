a36e3f754eedc6fd23a5c8993f4d2501242284c0
Rebin <muhammedrebin1794@gmail.com>
2024-10-16 23:39:46 +0530

    Problem on oop

---
+++ 19.OOP/problems/type_checking.py
@@ -0,0 +1,8 @@
+# Write a simple Python class named Student and display its type. 
+# Also, display the __dict__ attribute keys and the value of the __module__ attribute of the Student class.
+
+class Student:
+    pass  
+print(type(Student))
+print(Student.__dict__.keys())
+print(Student.__module__)
